c3d9bd2c8f895de9a0a03b03b23ca847211e2000
Dino-Scripts <86493204+KerreLProd@users.noreply.github.com>
2023-09-30 12:39:53 +0300

    Update script1.lua

--- user/3121-7197-D1N05-41UQ/script1.lua
+++ user/3121-7197-D1N05-41UQ/script1.lua
@@ -1,3 +1,7 @@
+local imgui = require("mimgui")
+
+local window, = new.bool(false)
+
 function main()
   while not isSampAvailable() do wait(0) end
   while not sampIsLocalPlayerSpawned() do wait(0) end
@@ -14,5 +18,19 @@ end
 function twoFunction()
   sampAddChatMessage("А эт кароч 2 функа в маин", -1)
 end
+
+imgui.OnFrame(
+  function() return window[0] end,
+  function(self)
+    local screenX, screenY = getScreenResolution()
+    local windowX, windowY = 600, 300
+    imgui.SetNextWindowPos(imgui.ImVec2(screenX/2, screenY/2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
+    imgui.SetNextWindowSize(imgui.ImVec2(windowX, windowY), imgui.Cond.FirstUseEver)
+    imgui.Begin("залупка", window)
+      imgui.Text("Рус")
+      imgui.Text("Eng")
+    imgui.End()
+  end
+)
 
 main()
